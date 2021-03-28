51c58e8f26df2f58c8521b744186736175753521
Shivam <iamshivamgp@gmail.com>
2021-03-28 21:40:36 +0530

    UI done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   return <>
+    <div className="container">
+      <div className="jumbotron my-5">
+        <h1 id="heading" className="display-4 text-center">Login</h1>
+        <form>
+          <div className="row form-group">
+            <label
+              id="name"
+              className="col-2 bg-warning mx-3 font-weight-bolder input-group-text"
+            >
+              Username:
+            </label>
+            <input type="text" className="form-control col-8 lead" />
+          </div>
+          <div className="row form-group">
+            <label
+              id="pass"
+              className="col-2  bg-warning mx-3 font-weight-bolder input-group-text"
+            >
+              Password:
+            </label>
+            <input type="password" className="form-control col-8 lead" />
+          </div>
+          <hr className="my-4" />
+          <div className="text-center">
+            <button type="submit" className="btn btn-danger btn-lg">
+              SUBMIT
+            </button>
+          </div>
+        </form> 
+      </div>
     </div>
-  );
+  </>
 }
 
 export default App;
+
